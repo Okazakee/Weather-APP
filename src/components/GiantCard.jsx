b39c2cba20fe1a46c31cd6a5b417e3ad0526f100
okazakee <cristiandcbusiness@gmail.com>
2023-05-20 18:07:41 +0200

    preps for image fetching

--- src/components/GiantCard.jsx
+++ src/components/GiantCard.jsx
@@ -1,9 +1,13 @@
 import { useContext } from 'react'
+import { createApi } from 'unsplash-js';
+
 import { StylesContext } from '@/contexts/StylesContext';
+import { WeatherDataContext } from '@/contexts/WeatherDataContext';
 
 export default function GiantCard() {
 
     const { GiantCardStyles } = useContext(StylesContext);
+    const { selectedCity } = useContext(WeatherDataContext);
 
 
   return (
